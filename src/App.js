7c2a323bedd32eb7cbab2daf5046e5b22790ee89
selvatico87 <ignacioselvatico@gmail.com>
2023-04-16 19:19:17 -0300

    16/4.3
    Trabajamos con rutas anidadas.
    Vimos cómo crear una segunda camada de filtros para mostrar artículos de una subcategoría específica, mejorando la experiencia del usuario al buscar un artículo.
    Usamos el hook useRouteMatch para crear caminos dinámicos.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Page404 from './pages/Page404';
 import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Post from './pages/Post';
+import Categoria from './pages/Categoria';
 
 function App() {
   
@@ -18,6 +19,7 @@ function App() {
         <Route path='/' element={<Home></Home>} />
         <Route path='/sobre' element={<Sobre></Sobre>} />
         <Route path='/posts/:id' element={<Post/>}/>
+        <Route path='/categoria/:id/*' element={<Categoria/>}/>
         <Route path='*' element={<Page404/>} />
       </Routes>
     </Router>
